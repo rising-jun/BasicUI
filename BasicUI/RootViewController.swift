6b29adbe590569a98d30929e1ff83a457feae329
kimdongjune <jun36817@naver.com>
2021-03-12 16:10:25 +0900

    added BaseClass

--- BasicUI/RootViewController.swift
+++ BasicUI/RootViewController.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 
-class RootViewController: UIViewController {
+class RootViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         // Do any additional setup after loading the view.
+        
+        
+        
     }
 
 
