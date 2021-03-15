f5f10befc442415cf475145d111f4c94864a7ddf
kimdongjune <jun36817@naver.com>
2021-03-15 14:42:40 +0900

    made class

---
+++ BasicUI/firstscreen/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BasicUI
+//
+//  Created by 김동준 on 2021/03/15.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+
+class FirstViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+        
+        
+        
+    }
+
+
+}
+
