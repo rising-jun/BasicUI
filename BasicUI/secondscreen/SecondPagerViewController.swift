21e31507d75144c14de60799e328b4091f1cd0d1
kimdongjune <jun36817@naver.com>
2021-03-15 15:05:25 +0900

    made secondView

---
+++ BasicUI/secondscreen/SecondPagerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondPagerViewController.swift
+//  BasicUI
+//
+//  Created by 김동준 on 2021/03/15.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+
+class SecondPagerViewController: BaseViewController{
+    lazy var secondViewPager = SecondViewPager(frame: view.frame)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view = secondViewPager
+        
+    }
+    
+}
---
+++ BasicUI/secondscreen/SecondViewPager.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewPager.swift
+//  BasicUI
+//
+//  Created by 김동준 on 2021/03/15.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewPager: BaseView{
+    override func setup() {
+        super.setup()
+        backgroundColor = .yellow
+    }
+}
